d044abb15c839b6989a9e8665fef0ff778317c71
RUAN0007 <ruanpingcheng@gmail.com>
2018-11-26 13:49:11 +0800

    Add node permission

--- network/quorum/init.sh
+++ network/quorum/init.sh
@@ -11,5 +11,6 @@ mkdir -p $QUO_DATA/dd$i/{keystore,geth}
 
 cp keys/key$i $QUO_DATA/dd$i/keystore
 cp raft/static-nodes$2.json $QUO_DATA/dd$i/static-nodes.json
+cp raft/static-nodes$2.json $QUO_DATA/dd$i/permissioned-nodes.json
 cp raft/nodekey$i $QUO_DATA/dd$i/geth/nodekey
 ${QUORUM} --datadir=$QUO_DATA/dd$i init $QUO_HOME/genesis_quorum.json
