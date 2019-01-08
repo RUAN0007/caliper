7089bfcb48e95396c1d45f37b57fc805856915fd
RUAN0007 <ruanpingcheng@gmail.com>
2019-01-08 14:10:36 +0800

    Avoid purge previous exp results for empty folder

--- benchmark/ycsb/fabric-env/sc.sh
+++ benchmark/ycsb/fabric-env/sc.sh
@@ -5,16 +5,19 @@ set -o pipefail # Save exit code after pipelining to tee
 
 for NODE_COUNT in 1 3 5 7 9 11; do
 # for NODE_COUNT in 1; do
-  EXP_DIR=/data/ruanpc/fabric-exp/scale/node-${NODE_COUNT}
-  mkdir -p ${EXP_DIR}
-  python ../../network/fabric/cluster/script/launch.py ../../network/fabric/cluster/env/${NODE_COUNT}p down;
-  for path in fabric-env/scale/node-${NODE_COUNT}/*; do
-    filename=$(basename -- "$path")
-    python ../../network/fabric/cluster/script/launch.py ../../network/fabric/cluster/env/${NODE_COUNT}p up;
-    echo "======================Experiment for ", $filename, "with ", $NODE_COUNT, "=========================="
-    timeout 120m node main.js -c $path -n ../../network/fabric/cluster/env/${NODE_COUNT}p/setup.json | tee ${EXP_DIR}/${filename}.log
-    echo "=============================================================================="
-
+  if [ -d fabric-env/scale/node-${NODE_COUNT} ] 
+  then
+    EXP_DIR=/data/ruanpc/fabric-exp/scale/node-${NODE_COUNT}
+    mkdir -p ${EXP_DIR}
     python ../../network/fabric/cluster/script/launch.py ../../network/fabric/cluster/env/${NODE_COUNT}p down;
-  done
+    for path in fabric-env/scale/node-${NODE_COUNT}/*; do
+      filename=$(basename -- "$path")
+      python ../../network/fabric/cluster/script/launch.py ../../network/fabric/cluster/env/${NODE_COUNT}p up;
+      echo "======================Experiment for ", $filename, "with ", $NODE_COUNT, "=========================="
+      timeout 120m node main.js -c $path -n ../../network/fabric/cluster/env/${NODE_COUNT}p/setup.json | tee ${EXP_DIR}/${filename}.log
+      echo "=============================================================================="
+
+      python ../../network/fabric/cluster/script/launch.py ../../network/fabric/cluster/env/${NODE_COUNT}p down;
+    done
+  fi
 done
--- benchmark/ycsb/quorum-env/scale.sh
+++ benchmark/ycsb/quorum-env/scale.sh
@@ -8,27 +8,30 @@ for NODE_COUNT in 1 3 5 7 9 11; do
   EXP_DIR=/data/ruanpc/quorum-exp/scale/node-${NODE_COUNT}
   mkdir -p ${EXP_DIR}
   ../../network/quorum/stop-all.sh 11
-  for path in quorum-env/sc-config/node-${NODE_COUNT}/*; do
-    filename=$(basename -- "$path")
-    ../../network/quorum/init-all.sh ${NODE_COUNT} -p; 
-    sleep 5;
-    ../../network/quorum/start-all.sh ${NODE_COUNT} -p;
-    echo "======================Experiment for $filename with $NODE_COUNT nodes=========================="
-    timeout 120m node main.js -c $path -n  ../../../../../data/ruanpc/qdata/setup.json | tee ${EXP_DIR}/${filename}.log
-    # rc=$?
-    # i=1
-    # while [[ $rc -ne 0 ]] && [[ $i -le $RETRY_LIMIT ]];
-    # do
-    #   echo "Retry..." $i
-    #   ../../network/quorum/stop-all.sh ${NODE_COUNT}; ../../network/quorum/init-all.sh ${NODE_COUNT} -p; ../../network/quorum/start-all.sh ${NODE_COUNT} -p;
-    #   node main.js -c $path -n  ../../../../../data/ruanpc/qdata/setup.json | tee ${EXP_DIR}/${filename}.log.${i}
-    #   rc=$?
-    #   let i=$i+1
-    # done
+  if [ -d quorum-env/sc-config/node-${NODE_COUNT} ] 
+  then
+    for path in quorum-env/sc-config/node-${NODE_COUNT}/*; do
+      filename=$(basename -- "$path")
+      ../../network/quorum/init-all.sh ${NODE_COUNT} -p; 
+      sleep 5;
+      ../../network/quorum/start-all.sh ${NODE_COUNT} -p;
+      echo "======================Experiment for $filename with $NODE_COUNT nodes=========================="
+      timeout 120m node main.js -c $path -n  ../../../../../data/ruanpc/qdata/setup.json | tee ${EXP_DIR}/${filename}.log
+      # rc=$?
+      # i=1
+      # while [[ $rc -ne 0 ]] && [[ $i -le $RETRY_LIMIT ]];
+      # do
+      #   echo "Retry..." $i
+      #   ../../network/quorum/stop-all.sh ${NODE_COUNT}; ../../network/quorum/init-all.sh ${NODE_COUNT} -p; ../../network/quorum/start-all.sh ${NODE_COUNT} -p;
+      #   node main.js -c $path -n  ../../../../../data/ruanpc/qdata/setup.json | tee ${EXP_DIR}/${filename}.log.${i}
+      #   rc=$?
+      #   let i=$i+1
+      # done
 
-    echo "=============================================================================="
+      echo "=============================================================================="
 
-    ../../network/quorum/stop-all.sh ${NODE_COUNT};
-  done
+      ../../network/quorum/stop-all.sh ${NODE_COUNT};
+    done
+  fi
 done
 
